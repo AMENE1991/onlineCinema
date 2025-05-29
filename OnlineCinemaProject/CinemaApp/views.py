ac1a923bb87a043f2395d6047377d482745f5624
Мои Чуваки <My dudes@step.lan>
2025-05-29 15:22:22 +0700

    Test

--- OnlineCinemaProject/CinemaApp/views.py
+++ OnlineCinemaProject/CinemaApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Film, Review
+from .serializers import FilmSerializer, ReviewSerializer
+
+
+class FilmListCreate(generics.ListCreateAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+class ReviewListCreate(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
 
 # Create your views here.
--- OnlineCinemaProject/OnlineCinemaProject/urls.py
+++ OnlineCinemaProject/OnlineCinemaProject/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from CinemaApp.views import FilmListCreate
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cinema/', FilmListCreate),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
