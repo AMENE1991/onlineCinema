5baea8817fb32727c24ab2f4f2b24619af45beec
Мои Чуваки <My dudes@step.lan>
2025-05-29 11:49:00 +0700

    Создал модель Фильма

--- OnlineCinemaProject/CinemaApp/models.py
+++ OnlineCinemaProject/CinemaApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField(help_text="Duration in minutes")
+    director = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    film_file = models.FileField(upload_to='films/')  # Поле для загрузки файла фильма
+
+    def __str__(self):
+        return self.title
+
