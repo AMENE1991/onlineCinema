7fdbc34c350d7799f02a1e202b5b78940980c263
Мои Чуваки <My dudes@step.lan>
2025-05-30 11:28:36 +0700

    Добавил описание(DetailView)

--- OnlineCinemaProject/CinemaApp/views.py
+++ OnlineCinemaProject/CinemaApp/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView
 from .models import Film, Review
 from .serializers import FilmSerializer, ReviewSerializer
 
 
-class FilmListCreate(generics.ListCreateAPIView):
+class FilmListCreate(ListCreateAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
 
-class ReviewListCreate(generics.ListCreateAPIView):
+class FilmDetailView(RetrieveAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+
+
+
+class ReviewListCreate(ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+class 
 
 # Create your views here.
